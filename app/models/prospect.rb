9b9db793900bd476437a8cb3ab9fc937948cef38
Daryl Richter <daryl@ngzax.com>
2018-04-23 21:01:12 -0400

    Add navigation between prospects.

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -1,2 +1,9 @@
 class Prospect < ApplicationRecord
+  def previous
+    self.class.where("id < #{self.id}").order("id desc").first
+  end
+
+  def next
+    self.class.where("id > #{self.id}").order("id asc").first
+  end
 end
