2507563326017b3b604c8e94a87dadca3ea81776
Daryl Richter <daryl@ngzax.com>
2018-04-25 14:53:41 -0400

    Add User Editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
 
   before_action :authenticate_user!
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def index
     @users = User.all
   end
@@ -10,6 +14,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     unless @user == current_user
       redirect_to root_path, :alert => "Access denied."
+    end
+  end
+
+  def update
+    params.permit!
+    @user = User.find(params[:id])
+
+    if params[:user]
+      if @user.update_attributes(params[:user])
+        flash[:notice] = "User was successfully updated!"
+        redirect_to :action => :show, :id => @user and return
+      else
+        render :action => :edit and return
+      end
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,20 @@
 class User < ApplicationRecord
 
-    enum role: [:user, :vip, :admin]
-
-    after_initialize :set_default_role, :if => :new_record?
-    validates_presence_of :email
-
-    def set_default_role
-      self.role ||= :user
-    end
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [:user, :vip, :admin]
+
+  after_initialize :set_default_role, :if => :new_record?
+  validates_presence_of :email
+
+  def set_default_role
+    self.role ||= :user
+  end
+
+  def to_s
+    #{self.name}
+  end
 
 end
